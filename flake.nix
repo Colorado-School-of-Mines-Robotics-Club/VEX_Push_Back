0e6156671b1d4e4af4426ba3341478073fb92a3b
Tyler Beckman <ty@myriation.xyz>
2026-02-09 14:30:55 -0700

    Remove remnants of RPI copro

--- flake.nix
+++ flake.nix
@@ -17,7 +17,6 @@
             url = "github:numtide/treefmt-nix";
             inputs.nixpkgs.follows = "nixpkgs";
         };
-        nixos-raspberrypi.url = "github:nvmd/nixos-raspberrypi/main";
     };
 
     outputs =
@@ -28,28 +27,9 @@
             rust-overlay,
             cargo-v5,
             treefmt-nix,
-            nixos-raspberrypi,
             ...
         }:
-        {
-            nixosConfigurations =
-                let
-                    mkPi =
-                        piNumber:
-                        nixos-raspberrypi.lib.nixosSystem {
-                            specialArgs = {
-                                inherit inputs self piNumber;
-                                inherit (inputs) nixos-raspberrypi;
-                            };
-                            modules = [ ./coprocessor/nixos ];
-                        };
-                in
-                {
-                    rpi1 = mkPi 1;
-                    rpi2 = mkPi 2;
-                };
-        }
-        // flake-utils.lib.eachDefaultSystem (
+        flake-utils.lib.eachDefaultSystem (
             system:
             let
                 # Import nixpkgs
@@ -61,16 +41,6 @@
                 # Setup formatter
                 treefmtCfg = lib.modules.importApply ./treefmt.nix { inherit rust'; };
                 treefmt = treefmt-nix.lib.evalModule pkgs treefmtCfg;
-                # Build extra python packages
-                mkPythonPackages = ps: rec {
-                    qwiic-i2c = pkgs.callPackage ./coprocessor/nixos/config/otos/packages/qwiic-i2c.nix {
-                        python3Packages = ps;
-                    };
-                    qwiic-otos = pkgs.callPackage ./coprocessor/nixos/config/otos/packages/qwiic-otos.nix {
-                        inherit qwiic-i2c;
-                        python3Packages = ps;
-                    };
-                };
                 # Make winit work on NixOS
                 libPath = with pkgs; lib.makeLibraryPath [
                     libGL
@@ -87,12 +57,6 @@
                         self.formatter.${system}
                     ]
                     ++ (with pkgs; [
-                        (python313.withPackages (
-                            pythonPackages: with pythonPackages; [
-                                cobs
-                                crc
-                            ]
-                        ))
                         evcxr
                         SDL2
                         mpremote
@@ -103,29 +67,6 @@
                 };
 
                 formatter = treefmt.config.build.wrapper;
-
-                packages.pythonWithLibs = pkgs.python313.withPackages (
-                    ps:
-                    (with ps; [
-                        cobs # For packet delimited serial
-                        pyserial # For serial communication
-                        crc # For message checksums
-                        smbus2 # For i2c communication
-                        rpi-gpio # For controlling GPIO pins
-                    ])
-                    ++ (with (mkPythonPackages ps); [
-                        # Add both of the vendor-specific i2c/otos libs
-                        qwiic-i2c
-                        qwiic-otos
-                    ])
-                );
             }
         );
-
-    nixConfig = {
-        extra-substituters = [ "https://nixos-raspberrypi.cachix.org" ];
-        extra-trusted-public-keys = [
-            "nixos-raspberrypi.cachix.org-1:4iMO9LXa8BqhU+Rpg6LQKiGa2lsNh/j2oiYLNOQ5sPI="
-        ];
-    };
 }
